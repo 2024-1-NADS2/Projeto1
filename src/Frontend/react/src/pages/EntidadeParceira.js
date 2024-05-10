84bd5402fbc2323e9349dad2a78be0be656f5c36
Leonardo de Souza <leonardosouzamouzinho4554@gmail.com>
2024-05-09 23:09:10 -0300

    LeoCommit
    Primeiro commit pagina entidades

--- src/Frontend/react/src/pages/EntidadeParceira.js
+++ src/Frontend/react/src/pages/EntidadeParceira.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import styled from 'styled-components';
+import FotoOng from '../components/FotoOng';
+
+
+const EntidadesContainer = styled.div`
+    @media only screen and (min-width: 1201px){
+        width: 100%;
+    }
+    @media (max-width: 1200px) and (min-width: 1025px){
+        width: 100%;
+    } 
+    @media (max-width: 1024px) and (min-width: 769px){
+        width: 100%;
+    } 
+    @media (max-width: 768px) and (min-width: 481px){
+        width: 100%;
+    } 
+    @media (max-width: 480px) and (min-width: 320px){
+        width: 100%;
+    } 
+    `
+    const H1 = styled.div`
+    text-align: center;
+
+    `
+    
+    
 
 function EntidadeParceira(){
     return(
         <div>
+            <EntidadesContainer>
             <Header/>
+            <H1>
+            <h1>Associação Mulheres Unidas Venceremos</h1>
+            </H1>
+            <FotoOng />
 
             <Footer/>
+            </EntidadesContainer>
+
         </div>
+        
     );
 }
 
