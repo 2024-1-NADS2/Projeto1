f26f1b33c37b007f5afa3a50928a20795c98a943
Leonardo de Souza <leonardosouzamouzinho4554@gmail.com>
2024-05-19 13:51:41 -0300

    ComitFaq
    Adicionado algumas perguntas na faq

--- src/Frontend/react/src/components/FaqSite.js
+++ src/Frontend/react/src/components/FaqSite.js
@@ -12,6 +12,25 @@ const FaqH1 = styled.h1`
     font-weight: bold;
     text-align:center;
 `
+const Sumary = styled.summary`
+    font-weight: bold;
+    padding: 20px;
+
+`
+const FaqH4 = styled.h4`
+    text-align: left;
+    font-weight: bold;
+    padding: 20px;
+    margin-top: 20px;
+`
+const Details = styled.details`
+    padding:10PX;
+    text-align: justify;
+`
+const P = styled.p`
+     padding: 20px;
+    text-align: justify;
+`
 
 
 
@@ -21,6 +40,27 @@ function Faq(){
 
         <FaqContainer>  
             <FaqH1> Dúvidas </FaqH1>
+            <FaqH4>Perguntas Frequentes</FaqH4>
+            <Details>
+             <Sumary>Como posso doar para uma Ong parceira</Sumary>   
+               <P>Para se inscrever como doador, faça seu cadastro em poucos passos no nosso site.</P> 
+            </Details>
+            <Details>
+            <Sumary>Como posso adicionar minha nota fiscal</Sumary>
+                    <P>oi</P>
+            </Details>
+
+            <Details>
+                <Sumary>Como sei que minha nota não esta com o CPF?</Sumary>
+                <P>oi</P>
+
+            </Details>
+
+            <Details>
+                <Sumary>Qual valor a ong ganha com cada nota?</Sumary>
+                <P>oi</P>
+
+            </Details>
 
 
 
