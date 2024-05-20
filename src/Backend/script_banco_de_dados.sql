aa28db1f7869638d67a519bfa7199c66309879b9
FabricioNasci <fabricio.c.nascimento@gmail.com>
2024-05-20 17:00:39 -0300

    Atualização BD

--- src/Backend/script_banco_de_dados.sql
+++ src/Backend/script_banco_de_dados.sql
@@ -5,38 +5,38 @@ CREATE DATABASE doamais;
 USE doamais;
 
 -- Cria as tabelas --
-
-CREATE TABLE usuarios (
+CREATE TABLE Usuarios (
     id_usuario INT AUTO_INCREMENT UNIQUE,
     nome VARCHAR(100) NOT NULL,
     senha VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL PRIMARY KEY,
     telefone VARCHAR(15),
-    tipo_pessoa INT NOT NULL  -- 0 para CPF, 1 para CNPJ --
+    tipo_pessoa INT NOT NULL  -- 0 para CPF, 1 para CNPJ
 );
 
-CREATE TABLE cupons (
-    id_cupons INT  AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Cupons (
+    id_cupom INT  AUTO_INCREMENT PRIMARY KEY,
     data_envio DATETIME NOT NULL,
     ong_beneficiada VARCHAR(100),
     valor_cupom DOUBLE NOT NULL,
     email VARCHAR(100),
-    CONSTRAINT Fkemail_usuario FOREIGN KEY (email) REFERENCES usuarios(email)
+    CONSTRAINT Fkid_usuario FOREIGN KEY (email) REFERENCES Usuarios(email)
 );
 
-CREATE TABLE ongs(
-	id_ong INT PRIMARY KEY,
-	senha VARCHAR(12) NOT NULL,
+CREATE TABLE ongs (
+    id_ong INT AUTO_INCREMENT UNIQUE,
+    nome_ong VARCHAR(100) PRIMARY KEY,
+    senha VARCHAR(255) NOT NULL,
     cnpj VARCHAR(14) NOT NULL,
-    nome_ong VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     telefone VARCHAR(18) NOT NULL
 );
 
-CREATE TABLE recebe(
-	documento VARCHAR(255) NOT NULL, -- Caminmho onde foi armazenado o cupons fiscal --
+CREATE TABLE recebe (
+    id_documento INT PRIMARY KEY AUTO_INCREMENT,
+    documento VARCHAR(500) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    id_ong INT NOT NULL,
-    CONSTRAINT Fkemail_doador FOREIGN KEY (email) REFERENCES usuarios(email),
-    CONSTRAINT Fkid_ong FOREIGN KEY (id_ong) REFERENCES ongs(id_ong)
+    nome_ong VARCHAR(100) NOT NULL,
+    CONSTRAINT Fkid_doador FOREIGN KEY (email) REFERENCES usuarios(email),
+    CONSTRAINT Fkid_ong FOREIGN KEY (nome_ong) REFERENCES ongs(nome_ong)
 );
